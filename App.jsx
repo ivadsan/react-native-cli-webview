3bdae3a0eafb37bb1206452b32f8cab2931c65f4
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2023-08-15 10:40:55 -0500

    custom html for test

--- App.jsx
+++ App.jsx
@@ -18,9 +18,17 @@ const styles = StyleSheet.create({
   },
 });
 
+const html = `
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <body style="display:flex; flex-direction: column; flex: 1; justify-content: center; align-items: center; cursor: pointer">
+    <h1>WebView</h1>
+    <a id="open-pref" style="text-align: center; background: lightblue; text-decoration: none; padding: 8px; border-radius: 8px; font-weight: bold" href="https://www.mercadopago.com.ar/checkout/v1/redirect?preference-id=239658604-49220b41-cdfe-4dd0-b41c-bf806e95ed3c&device-override=mobile">Open preference</a>
+  </body>
+`;
+
 function App() {
   const runFirst = `
-      const backButton = document.querySelector("#group_content > div.group-back-url > a")
+      const backButton = document.querySelector("#open-pref")
       backButton.addEventListener('click', function(e){
         e.preventDefault()
         e.stopPropagation()
@@ -35,7 +43,7 @@ function App() {
         originWhitelist={['*']}
         style={styles.webview}
         source={{
-          uri: 'https://www.mercadopago.com.ar/checkout/v1/redirect?preference-id=239658604-49220b41-cdfe-4dd0-b41c-bf806e95ed3c&device-override=mobile',
+          html,
         }}
         injectedJavaScript={runFirst}
       />
